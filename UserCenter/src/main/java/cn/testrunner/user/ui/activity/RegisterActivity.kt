70b4c0bc5670215491de71b7dce2035b01c1689c
lujichao <lengaoxingchen@qq.com>
2020-08-22 21:17:41 +0800

    Initial commit

---
+++ UserCenter/src/main/java/cn/testrunner/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package cn.testrunner.user.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.testrunner.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/cn/testrunner/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.testrunner.mall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.testrunner.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
